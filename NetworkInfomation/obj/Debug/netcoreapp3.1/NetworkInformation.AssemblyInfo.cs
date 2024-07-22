a5b6dee73fc400255ab782ad58eb6a0a88c59e22
jlrios <rdjl5791@gmail.com>
2024-07-22 00:48:55 -0600

    Temp.

--- NetworkInfomation/obj/Debug/netcoreapp3.1/NetworkInformation.AssemblyInfo.cs
+++ NetworkInfomation/obj/Debug/netcoreapp3.1/NetworkInformation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetworkInformation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee035550c2d1b7804a65bfc8b43a3cb55d5335e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d293d0f0f2366e210b946cbbae09891be99cd7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetworkInformation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetworkInformation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
